79dc1bc20fe79f5d0e8c3a833e66d5db44aad16b
Jared A Barneck <rhyous@yahoo.com>
2019-12-17 11:03:04 -0700

    Added some additional examples

---
+++ src/AutofacGenericExample/Program.cs
@@ -0,0 +1,148 @@
+﻿using Autofac;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutofacGenericExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterGeneric(typeof(ServiceLoader<>)).As(typeof(IServiceLoader<>));
+            builder.RegisterGeneric(typeof(ServiceProxy<,,>)).As(typeof(IService<,,>));
+            var container = builder.Build();
+
+            var proxyUserService = container.Resolve<IService<Organization, IOrganization, int>>();
+            var user = proxyUserService.Get(27);
+            var proxyOrganizationService = container.Resolve<IService<User, IUser, int>>();
+            var org = proxyOrganizationService.Get(1011);
+        }
+    }
+
+    public interface IBaseEntity<TId>
+    {
+        TId Id { get; set; }
+    }
+
+    public interface IUser : IBaseEntity<int>
+    {
+        string Username { get; set; }
+    }
+
+    public class User : IUser
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+    }
+
+    public interface IOrganization : IBaseEntity<int>
+    {
+        string Name { get; set; }
+    }
+
+    public class Organization : IOrganization
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+
+    public interface IService<TEntity, TInterface, TId>
+        where TEntity : class, TInterface, new()
+        where TInterface : IBaseEntity<TId>
+        where TId : IComparable, IComparable<TId>, IEquatable<TId>
+    {
+        TInterface Get(TId id);
+    }
+
+    public class Service<TEntity, TInterface, TId> : IService<TEntity, TInterface, TId>
+        where TEntity : class, TInterface, new()
+        where TInterface : IBaseEntity<TId>
+        where TId : IComparable, IComparable<TId>, IEquatable<TId>
+    {
+        public virtual TInterface Get(TId id)
+        {
+            return new TEntity { Id = id };
+        }
+    }
+
+    public class ServiceProxy<TEntity, TInterface, TId> : IService<TEntity, TInterface, TId>
+        where TEntity : class, TInterface, new()
+        where TInterface : IBaseEntity<TId>
+        where TId : IComparable, IComparable<TId>, IEquatable<TId>
+    {
+        private readonly IServiceLoader<IService<TEntity, TInterface, TId>> _ServiceLoader;
+
+        public ServiceProxy(IServiceLoader<IService<TEntity, TInterface, TId>> serviceLoader)
+        {
+            _ServiceLoader = serviceLoader;
+        }
+
+        internal IService<TEntity, TInterface, TId> Service => _Service ?? (_Service = _ServiceLoader.Load());
+        private IService<TEntity, TInterface, TId> _Service;
+
+        public TInterface Get(TId id)
+        {
+            return Service.Get(id);
+        }
+    }
+
+    public interface IServiceLoader<T>
+    {
+        T Load();
+    }
+
+    public class ServiceLoader<T> : IServiceLoader<T>
+            where T : class
+    {
+        private readonly ILifetimeScope _Container;
+
+        public ServiceLoader(ILifetimeScope container)
+        {
+            _Container = container;
+        }
+        public T Load()
+        {
+            using (var scope = _Container.BeginLifetimeScope(builder =>
+            {
+                var types = new[] { typeof(UserService), typeof(Service<Organization, IOrganization, int>) };
+                foreach (var type in types)
+                {
+
+                    if (!type.IsGenericTypeDefinition)
+                        builder.RegisterType(type).As(type.GetInterfaces().FirstOrDefault());
+                    else
+                        builder.RegisterGeneric(type.GetGenericTypeDefinition()).As(type.GetInterfaces().FirstOrDefault());
+
+                }
+            }))
+            {
+                if (typeof(T).IsGenericTypeDefinition)
+                    return null;
+                return scope.Resolve<T>(); ;
+            }
+        }
+    }
+
+    public class ServiceExtended<TEntity, TInterface, TId> : Service<TEntity, TInterface, TId>
+    where TEntity : class, TInterface, new()
+    where TInterface : IBaseEntity<TId>
+    where TId : IComparable, IComparable<TId>, IEquatable<TId>
+    {
+        public virtual TInterface Set(TEntity entity)
+        {
+            return entity;
+        }
+    }
+
+    public class UserService : Service<User, IUser, int>
+    {
+        public override IUser Get(int id)
+        {
+            return new User();
+        }
+    }
+}
---
+++ src/AutofacScopeExample/AutofacScopeExample/Program.cs
@@ -0,0 +1,133 @@
+﻿using Autofac;
+using System;
+
+namespace AutofacScopeExample
+{
+    public abstract class BaseService
+    {
+        private readonly int _instanceNumber;
+
+        public BaseService(int instance)
+        {
+            _instanceNumber = instance;
+            Console.WriteLine($"Just created {this.GetType().Name} #{_instanceNumber}");
+        }
+
+        public void Dispose() => Console.WriteLine($"Disposing {this.GetType().Name} #{_instanceNumber}");
+    }
+
+    public class TransientService : BaseService 
+    {
+        public static int _instanceCount;
+        public TransientService() : base(++_instanceCount) { }
+    }
+
+    public class LifetimeScopeService : BaseService
+    {
+        public static int _instanceCount;
+        public LifetimeScopeService() : base(++_instanceCount) { }
+    }
+
+    public class SingletonService : BaseService
+    {
+        public static int _instanceCount;
+        public SingletonService() : base(++_instanceCount) { }
+    }
+
+    public class OuterScopedSingletonService : BaseService
+    {
+        public static int _instanceCount;
+        public OuterScopedSingletonService() : base(++_instanceCount) { }
+    }
+
+    public class InnerScopedSingletonService : BaseService
+    {
+        public static int _instanceCount;
+        public InnerScopedSingletonService() : base(++_instanceCount) { }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var builder = new ContainerBuilder();
+
+            builder
+                .RegisterType<TransientService>()
+                .AsSelf()
+                .InstancePerDependency();
+
+            builder
+                .RegisterType<LifetimeScopeService>()
+                .AsSelf()
+                .InstancePerLifetimeScope();
+
+            builder
+                .RegisterType<SingletonService>()
+                .AsSelf()
+                .SingleInstance();
+
+            using (var container = builder.Build())
+            {
+                Console.WriteLine("Created the root scope");
+
+                var rootTransientService = container.Resolve<TransientService>();
+                var rootLifetimeScopeService = container.Resolve<LifetimeScopeService>();
+                var rootSingletonService = container.Resolve<SingletonService>();
+
+                var rootTransientServiceTwo = container.Resolve<TransientService>();
+                var rootLifetimeScopeServiceTwo = container.Resolve<LifetimeScopeService>();
+                var rootSingletonServiceTwo = container.Resolve<SingletonService>();
+
+                using (var outerLifetimeScope = container.BeginLifetimeScope(outerBuilder =>
+                {
+                    outerBuilder.RegisterType<OuterScopedSingletonService>().AsSelf().SingleInstance();
+                }))
+                {
+                    Console.WriteLine("Created the outer lifetime scope");
+
+                    var outerTransientService = outerLifetimeScope.Resolve<TransientService>();
+                    var outerLifetimeScopeService = outerLifetimeScope.Resolve<LifetimeScopeService>();
+                    var outerSingletonService = outerLifetimeScope.Resolve<SingletonService>();
+                    var outerOuterScopedSingletonService = outerLifetimeScope.Resolve<OuterScopedSingletonService>();
+
+                    var outerTransientServiceTwo = outerLifetimeScope.Resolve<TransientService>();
+                    var outerLifetimeScopeServiceTwo = outerLifetimeScope.Resolve<LifetimeScopeService>();
+                    var outerSingletonServiceTwo = outerLifetimeScope.Resolve<SingletonService>();
+                    var outerOuterScopedSingletonServiceTwo = outerLifetimeScope.Resolve<OuterScopedSingletonService>();
+
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        using (var innerLifetimeScope = outerLifetimeScope.BeginLifetimeScope(innerBuilder =>
+                        {
+                            innerBuilder.RegisterType<InnerScopedSingletonService>().AsSelf().SingleInstance();
+                        }))
+                        {
+                            Console.WriteLine($"Created the inner lifetime scope {i}");
+
+                            var innerTransientService = innerLifetimeScope.Resolve<TransientService>();
+                            var innerLifetimeScopeService = innerLifetimeScope.Resolve<LifetimeScopeService>();
+                            var innerSingletonService = innerLifetimeScope.Resolve<SingletonService>();
+                            var innerOuterScopedSingletonService = innerLifetimeScope.Resolve<OuterScopedSingletonService>();
+                            var innerInnerScopedSingletonService = innerLifetimeScope.Resolve<InnerScopedSingletonService>();
+
+                            var innerTransientServiceTwo = innerLifetimeScope.Resolve<TransientService>();
+                            var innerLifetimeScopeServiceTwo = innerLifetimeScope.Resolve<LifetimeScopeService>();
+                            var innerSingletonServiceTwo = innerLifetimeScope.Resolve<SingletonService>();
+                            var innerOuterScopedSingletonServiceTwo = innerLifetimeScope.Resolve<OuterScopedSingletonService>();
+                            var innerInnerScopedSingletonServiceTwo = innerLifetimeScope.Resolve<InnerScopedSingletonService>();
+                        }
+                        Console.WriteLine("Disposed the inner lifetime scope");
+                    }
+
+                }
+
+                Console.WriteLine("Disposed the outer lifetime scope");
+            }
+
+            Console.WriteLine("Disposed the root scope");
+
+            Console.ReadLine();
+        }
+    }
+}
